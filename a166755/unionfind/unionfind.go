984a65d3e0af0a25e20d215a110de801a9689f32
Mark Gritter <mgritter@gmail.com>
2020-12-02 16:28:09 -0600

    Switch union-find to a Cell type.

--- a166755/unionfind/unionfind.go
+++ a166755/unionfind/unionfind.go
@@ -1,23 +1,31 @@
 package unionfind
 
+// Identify a cell by its layer (size of the grid)
+// and position (counting from lower-left to upper-right
+// around the border.)
+type Cell struct {
+	Layer    int
+	Position int
+}
+
 type UnionFind struct {
-	parent map[string]string
-	rank   map[string]int
+	parent map[Cell]Cell
+	rank   map[Cell]int
 }
 
 func NewUnionFind() *UnionFind {
 	return &UnionFind{
-		parent: make(map[string]string),
-		rank:   make(map[string]int),
+		parent: make(map[Cell]Cell),
+		rank:   make(map[Cell]int),
 	}
 }
 
-func (u *UnionFind) MakeSet(key string) {
+func (u *UnionFind) MakeSet(key Cell) {
 	u.parent[key] = key
 	u.rank[key] = 0
 }
 
-func (u *UnionFind) Find(key string) string {
+func (u *UnionFind) Find(key Cell) Cell {
 	if u.parent[key] == key {
 		return key
 	}
@@ -27,7 +35,7 @@ func (u *UnionFind) Find(key string) string {
 	return p
 }
 
-func (u *UnionFind) Union(a string, b string) {
+func (u *UnionFind) Union(a Cell, b Cell) {
 	aRoot := u.Find(a)
 	bRoot := u.Find(b)
 	aRank := u.rank[aRoot]
--- a166755/unionfind/unionfind_test.go
+++ a166755/unionfind/unionfind_test.go
@@ -1,42 +1,171 @@
 package unionfind
 
 import (
+	"math"
 	"testing"
-	/*
-		"github.com/leanovate/gopter"
-		"github.com/leanovate/gopter/gen"
-		"github.com/leanovate/gopter/prop"
-	*/)
+
+	"github.com/leanovate/gopter"
+	"github.com/leanovate/gopter/gen"
+	"github.com/leanovate/gopter/prop"
+)
 
 func TestUnionFind_Example(t *testing.T) {
-	labels := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "o", "u"}
+	//   AB
+	//   BB
+	// ABCC
+	// AAAC
 	uf := NewUnionFind()
-	for i := range labels {
-		uf.MakeSet(labels[i])
+	for i := -2; i <= 2; i++ {
+		uf.MakeSet(Cell{3, i})
+	}
+	for i := -3; i <= 3; i++ {
+		uf.MakeSet(Cell{4, i})
 	}
 
-	uf.Union("a", "e")
-	uf.Union("i", "o")
-	uf.Union("o", "u")
-	uf.Union("a", "u")
+	// A
+	uf.Union(Cell{3, -2}, Cell{3, 2})
+	uf.Union(Cell{4, -3}, Cell{4, -2})
+	uf.Union(Cell{4, -1}, Cell{4, -2})
+	uf.Union(Cell{4, -3}, Cell{3, -2})
 
-	uf.Union("b", "c")
-	uf.Union("d", "f")
-	uf.Union("f", "g")
-	uf.Union("c", "h")
-	uf.Union("j", "b")
-	uf.Union("g", "j")
+	// B
+	uf.Union(Cell{3, -1}, Cell{3, 1})
+	uf.Union(Cell{3, 1}, Cell{4, 2})
+	uf.Union(Cell{4, 3}, Cell{4, 2})
 
-	vowelRoot := uf.Find("a")
-	consonantRoot := uf.Find("b")
-	for _, vowel := range []string{"a", "e", "i", "o", "u"} {
-		if uf.Find(vowel) != vowelRoot {
-			t.Fatalf("%q not in set %q", vowel, vowelRoot)
+	// C
+	uf.Union(Cell{4, 0}, Cell{4, 1})
+	uf.Union(Cell{3, 0}, Cell{4, 1})
+
+	checkCells := func(set []Cell) {
+		t.Helper()
+		origin := uf.Find(set[0])
+		for i := 1; i < len(set); i++ {
+			if uf.Find(set[i]) != origin {
+				t.Errorf("%v not in %v", set[i], origin)
+			}
 		}
 	}
-	for _, consonant := range []string{"b", "c", "d", "f", "g", "h", "j"} {
-		if uf.Find(consonant) != consonantRoot {
-			t.Fatalf("%q not in set %q", consonant, consonantRoot)
+
+	checkCells([]Cell{
+		Cell{3, 2},
+		Cell{3, -2},
+		Cell{4, -3},
+		Cell{4, -2},
+		Cell{4, -1},
+	})
+
+	checkCells([]Cell{
+		Cell{4, 0},
+		Cell{3, 0},
+		Cell{4, 1},
+	})
+
+	checkCells([]Cell{
+		Cell{3, -1},
+		Cell{3, 1},
+		Cell{4, 2},
+		Cell{4, 3},
+	})
+}
+
+func TestUnionFind_DoubleRing(t *testing.T) {
+	// Break the elements into two pieces
+	// Shuffle each half and join A to A+1 in each forming
+	// two cycles.
+	// Then bridge the two cycles to form the final set.
+	// Do this twice to get two independent sets.
+
+	n := 25
+
+	nextCell := func(c Cell) Cell {
+		return Cell{
+			Layer:    c.Layer,
+			Position: (c.Position + 1) % n,
 		}
 	}
+
+	// shuffle should be 4n+4 numbers
+	invariant := func(shuffle []int) bool {
+		a := make([]Cell, n)
+		b := make([]Cell, n)
+		c := make([]Cell, n)
+		d := make([]Cell, n)
+		uf := NewUnionFind()
+		for i := range a {
+			a[i] = Cell{1, i}
+			b[i] = Cell{2, i}
+			c[i] = Cell{3, i}
+			d[i] = Cell{4, i}
+			uf.MakeSet(a[i])
+			uf.MakeSet(b[i])
+			uf.MakeSet(c[i])
+			uf.MakeSet(d[i])
+		}
+
+		for i := 0; i < n; i++ {
+			remaining := n - i
+			a_choice := shuffle[i*4+0] % remaining
+			b_choice := shuffle[i*4+1] % remaining
+			c_choice := shuffle[i*4+0] % remaining
+			d_choice := shuffle[i*4+0] % remaining
+
+			// Move the one we've selected to the end of the list
+			uf.Union(a[a_choice], nextCell(a[a_choice]))
+			a[remaining-1], a[a_choice] = a[a_choice], a[remaining-1]
+
+			uf.Union(b[b_choice], nextCell(b[b_choice]))
+			b[remaining-1], b[b_choice] = b[b_choice], b[remaining-1]
+
+			uf.Union(c[c_choice], nextCell(c[c_choice]))
+			c[remaining-1], c[c_choice] = c[c_choice], c[remaining-1]
+
+			uf.Union(d[d_choice], nextCell(d[d_choice]))
+			d[remaining-1], d[d_choice] = d[d_choice], d[remaining-1]
+		}
+
+		uf.Union(a[shuffle[n*4+1]%n], b[shuffle[n*4+1]%n])
+		uf.Union(c[shuffle[n*4+1]%n], d[shuffle[n*4+1]%n])
+
+		aRoot := uf.Find(a[0])
+		for _, cell := range a {
+			if uf.Find(cell) != aRoot {
+				t.Logf("%v is not in %v", cell, aRoot)
+				return false
+			}
+		}
+		for _, cell := range b {
+			if uf.Find(cell) != aRoot {
+				t.Logf("%v is not in %v", cell, aRoot)
+				return false
+			}
+		}
+
+		cRoot := uf.Find(c[0])
+		for _, cell := range c {
+			if uf.Find(cell) != cRoot {
+				t.Logf("%v is not in %v", cell, aRoot)
+				return false
+			}
+		}
+		for _, cell := range d {
+			if uf.Find(cell) != cRoot {
+				t.Logf("%v is not in %v", cell, aRoot)
+				return false
+			}
+		}
+
+		if aRoot == cRoot {
+			t.Logf("roots are the same: %v", aRoot)
+			return false
+		}
+		return true
+	}
+
+	properties := gopter.NewProperties(nil)
+	properties.Property("shuffle order",
+		prop.ForAll(invariant,
+			gen.SliceOfN(4*n+4, gen.IntRange(0, math.MaxInt32)),
+		))
+	properties.TestingRun(t)
 }
